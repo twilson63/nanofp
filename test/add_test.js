25656c40f7a42bcc07dd79d45511c2629e0119c1
CamIAm <rcmontgo@gmail.com>
2017-10-03 09:00:25 -0400

    tab spaces adjusted

--- test/add_test.js
+++ test/add_test.js
@@ -2,7 +2,7 @@ import { add } from '../'
 import test from 'tape'
 
 test('add', t => {
-    t.equals(add(2, 3), 5)
-    t.equals(add(-2, -3), -5)
-    t.end()
+  t.equals(add(2, 3), 5)
+  t.equals(add(-2, -3), -5)
+  t.end()
 })
